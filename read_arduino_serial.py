bc5b3235e78ea92900193d92a9cfc2f35b852b41
Ian <ian.d.taulli@gmail.com>
2019-06-12 07:14:56 -0500

    testing

--- read_arduino_serial.py
+++ read_arduino_serial.py
@@ -14,46 +14,53 @@ def getparticles():
     diam is in micrometers
     """
     
-    output = [10000]*12
+    output = ['fail']*12
     diamlist = ['0.30','0.50','1.00','2.50','5.00','10.00']
+    max_tries = 10
     
-    for i in range(14):
+    #function will retry if filling the list fails
+    for attempt in range(max_tries):
         
-        data=ser.readline()
-        temp = data.split()
-        print("i is {}".format(i))
-        print("raw temp is")
-        print(temp)
+        #this for loop fills the list
+        for i in range(14):
         
-        if len(temp)==2:
-            if temp[0].isdigit() and (temp[1] in diamlist):
-                print("temp has passed")
-                count = int(temp[0])
-                diam = float(temp[1])
+            data=ser.readline()
+            temp = data.split()
+        
+            if len(temp)==2:
+                if temp[0].isdigit() and (temp[1] in diamlist):
+                    count = int(temp[0])
+                    diam = float(temp[1])
             
-                if diam==0.3:
-                    output[0] = count
-                    output[1] = diam
-                if diam==0.5:
-                    output[2] = count
-                    output[3] = diam
-                if diam==1.0:
-                    output[4] = count
-                    output[5] = diam
-                if diam==2.5:
-                    output[6] = count
-                    output[7] = diam
-                if diam==5.0:
-                    output[8] = count
-                    output[9] = diam
-                if diam==10.0:
-                    output[10] = count
-                    output[11] = diam
-    
-    return output
+                    if diam==0.3:
+                        output[0] = count
+                        output[1] = diam
+                    if diam==0.5:
+                        output[2] = count
+                        output[3] = diam
+                    if diam==1.0:
+                        output[4] = count
+                        output[5] = diam
+                    if diam==2.5:
+                        output[6] = count
+                        output[7] = diam
+                    if diam==5.0:
+                        output[8] = count
+                        output[9] = diam
+                    if diam==10.0:
+                        output[10] = count
+                        output[11] = diam
+        
+        if 'fail' in output:
+            print('failure')
+            time.sleep(1)
+        else:
+            print('success on attempt {}'.format(attempt))
+            return output
+            break
 
 while True:
     out = getparticles()
     print(out)
-    time.sleep(20)
+    time.sleep(5)
             
